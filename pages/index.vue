81e53cbfe9c51ec0be8b10262f2ead4d302494a0
Гвозди Заржавели <annuananipon@njs.netlab.cz>
2020-08-05 18:59:11 +0300

    feat: test leaflet library

---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-app>
+        <LMap
+            :zoom="17"
+            :center="[
+                53.4367995,
+                34.2885255
+            ]"
+        >
+            <LTileLayer
+                url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            />
+            <LMarker
+                :lat-lng="[
+                    53.4367995,
+                    34.2885255
+                ]"
+            >
+                <LTooltip>оз. Круглое</LTooltip>
+            </LMarker>
+        </LMap>
+    </v-app>
+</template>
+
+<script>
+import {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LTooltip
+} from 'vue2-leaflet';
+
+export default {
+    components: {
+        LMap,
+        LTileLayer,
+        LMarker,
+        LTooltip
+    }
+}
+</script>
+
+<style>
+@import url('leaflet/dist/leaflet.css');
+
+.wrapper
+{
+    width: 100%;
+    height: 100%;
+}
+</style>
